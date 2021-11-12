941077c994777407d2087ed01d44fd52fbd8511b
LostManJohnny <mrmatias1997@gmail.com>
2021-11-12 06:52:15 -0600

    Create edit functionality for admins

---
+++ api/edit_user.php
@@ -0,0 +1,14 @@
+<?php
+//Require authentication before proceeding with user update
+require('./authenticate.php');
+
+//Authentication passes, get database connection
+require('./connection.php');
+
+session_start();
+
+echo '<pre>';
+echo print_r($_up);
+echo print_r($_POST);
+echo print_r($_SESSION);
+echo '</pre>';
